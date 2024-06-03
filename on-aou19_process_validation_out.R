ada3df94d154a6da4f49c79685018fc4e07370af
idgregga7100 <57689906+idgregga7100@users.noreply.github.com>
2024-06-03 12:26:31 -0500

    Update on-aou19_process_validation_out.R: concat all groups

--- on-aou19_process_validation_out.R
+++ on-aou19_process_validation_out.R
@@ -44,57 +44,14 @@ head(pc_df)
 
 #SCORES SECTION
 
-#this whole thing per group k oof
-for(k in 1:9){
-#Read and combine PRS's (add up chr scores per population)
-scoredir="scores_10fold/"
-n=20809 #number of people in .sscore files
-#make matrix to add each pop's PRS to
-all_prs = matrix(nrow=n,ncol=3) #people x #pops
+#this whole thing per group k then rbind/concat all groups
 pops = c("AFR","AMR","EUR")
-for(i in 1:length(pops)){
-  pop = pops[i]
-  #make matrix to add each chr's score to
-  prs = matrix(nrow=n,ncol=22) # #people x #chromosomes
-  #load matrix
-  for(j in 1:22){
-    #read in scores calculated in held-out AoU (these were trained in AoU with SNPs in METS756 .bim file)
-    scores = fread(scoredir %&% "AoU_scaled_bmi_" %&% pop %&% "_pst_eff_a1_b0.5_phi1e-04_chr" %&% j %&% "_group"%&%k%&%"_heldout.sscore")
-    prs[,j] = scores$SCORE1_AVG
-  }
-  sum_prs = scale(rowSums(prs)) #take the sum of each row and scale (mean=0,var=1) to generate final PRS
-  #add to pop matrix
-  all_prs[,i] = sum_prs
-  #add sample IID's as rownames
-  rownames(all_prs) = scores$`#IID`
-  #add pops as colnames
-  colnames(all_prs) = pops
-}
-
-#join height PRS's with ancestry PCs
-prs_df = data.frame(all_prs) |> mutate(research_id=rownames(all_prs))
-head(prs_df)
-
-prs_pcs = inner_join(prs_df,pc_df,by='research_id')
-head(prs_pcs)
-dim(prs_pcs)
-
-#join PRS and PCs w/phenotypes
-#need person_id in pheno to be characters
-pheno = mutate(pheno,research_id=as.character(person_id))
-all_data = inner_join(prs_pcs,pheno,by='research_id')
-head(all_data) 
-colnames(all_data)
-#write all_data to text file.
-fwrite(all_data,"AoU_scaled_bmi_PRSCSx_phi1e-04_in_AoU_group"%&%k%&%"_held-out_w_pheno.txt",quote=F,row.names=F,sep='\t')
-#cp to bucket
-system("gsutil -m cp AoU_scaled_bmi_PRSCSx_phi1e-04_in_AoU_group"%&%k%&%"_held-out_w_pheno.txt ${WORKSPACE_BUCKET}/data/")
-}
+totalprs<-data.frame()
 
 #group 10 separately because group 10 n=20820
 k=10
 #Read and combine PRS's (add up chr scores per population)
-scoredir="scores_10fold/"
+scoredir="scores_10fold_1e-2/"
 n=20820 #number of people in .sscore files
 #make matrix to add each pop's PRS to
 all_prs = matrix(nrow=n,ncol=3) #people x #pops
@@ -106,7 +63,7 @@ for(i in 1:length(pops)){
   #load matrix
   for(j in 1:22){
     #read in scores calculated in held-out AoU (these were trained in AoU with SNPs in METS756 .bim file)
-    scores = fread(scoredir %&% "AoU_scaled_bmi_" %&% pop %&% "_pst_eff_a1_b0.5_phi1e-04_chr" %&% j %&% "_group"%&%k%&%"_heldout.sscore")
+    scores = fread(scoredir %&% "AoU_scaled_bmi_" %&% pop %&% "_pst_eff_a1_b0.5_phi1e-02_chr" %&% j %&% "_group"%&%k%&%"_heldout.sscore")
     prs[,j] = scores$SCORE1_AVG
   }
   sum_prs = scale(rowSums(prs)) #take the sum of each row and scale (mean=0,var=1) to generate final PRS
@@ -132,8 +89,54 @@ pheno = mutate(pheno,research_id=as.character(person_id))
 all_data = inner_join(prs_pcs,pheno,by='research_id')
 head(all_data) 
 colnames(all_data)
-#write all_data to text file.
-fwrite(all_data,"AoU_scaled_bmi_PRSCSx_phi1e-04_in_AoU_group"%&%k%&%"_held-out_w_pheno.txt",quote=F,row.names=F,sep='\t')
-#cp to bucket
-system("gsutil -m cp AoU_scaled_bmi_PRSCSx_phi1e-04_in_AoU_group"%&%k%&%"_held-out_w_pheno.txt ${WORKSPACE_BUCKET}/data/")
+totalprs<-all_data
 
+for(k in 1:9){
+  #Read and combine PRS's (add up chr scores per population)
+  scoredir="scores_10fold_1e-2/"
+  n=20809 #number of people in .sscore files
+  #make matrix to add each pop's PRS to
+  all_prs = matrix(nrow=n,ncol=3) #people x #pops
+  
+  for(i in 1:length(pops)){
+    pop = pops[i]
+    #make matrix to add each chr's score to
+    prs = matrix(nrow=n,ncol=22) # #people x #chromosomes
+    #load matrix
+    for(j in 1:22){
+      #read in scores calculated in held-out AoU (these were trained in AoU with SNPs in METS756 .bim file)
+      scores = fread(scoredir %&% "AoU_scaled_bmi_" %&% pop %&% "_pst_eff_a1_b0.5_phi1e-02_chr" %&% j %&% "_group"%&%k%&%"_heldout.sscore")
+      prs[,j] = scores$SCORE1_AVG
+    }
+    sum_prs = scale(rowSums(prs)) #take the sum of each row and scale (mean=0,var=1) to generate final PRS
+    #add to pop matrix
+    all_prs[,i] = sum_prs
+    #add sample IID's as rownames
+    rownames(all_prs) = scores$`#IID`
+    #add pops as colnames
+    colnames(all_prs) = pops
+  }
+  
+  #join height PRS's with ancestry PCs
+  prs_df = data.frame(all_prs) |> mutate(research_id=rownames(all_prs))
+  head(prs_df)
+  
+  prs_pcs = inner_join(prs_df,pc_df,by='research_id')
+  head(prs_pcs)
+  dim(prs_pcs)
+  
+  #join PRS and PCs w/phenotypes
+  #need person_id in pheno to be characters
+  pheno = mutate(pheno,research_id=as.character(person_id))
+  all_data = inner_join(prs_pcs,pheno,by='research_id')
+  head(all_data) 
+  colnames(all_data)
+  totalprs<-rbind(totalprs,all_data)
+ }
+
+
+
+#write all_data to text file.
+fwrite(totalprs,"AoU_scaled_bmi_PRSCSx_phi1e-02_in_AoU_w_pheno_6-3-24.txt",quote=F,row.names=F,sep='\t')
+#cp to bucket
+system("gsutil -m cp AoU_scaled_bmi_PRSCSx_phi1e-02_in_AoU_w_pheno_6-3-24.txt ${WORKSPACE_BUCKET}/data/")
